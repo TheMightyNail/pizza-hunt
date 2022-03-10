c25c3f10503ae2ec6b4a37c07d57bd1c89899944
Ian Linton <ilinton2571@gmail.com>
2022-03-10 14:34:25 -0700

    add reply routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment, 
+    removeComment,
+    addReply,
+    removeReply
+ } = require('../../controllers/comment-controller');
 
 module.exports = router;
 
@@ -7,6 +12,9 @@ module.exports = router;
 router.route('/:pizzaId').post(addComment);
 
 // /api/comments/pizzaId/commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
 
 module.exports = router;
